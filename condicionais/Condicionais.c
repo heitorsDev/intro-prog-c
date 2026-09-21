591b3a3f81b29c3db2f797d7ff6c394e3444f7eb
HeitorSantos-aignosi <heitor.santos@aignosi.com.br>
2026-09-21 12:51:23 -0300

    865

--- condicionais/Condicionais.c
+++ condicionais/Condicionais.c
@@ -708,6 +708,31 @@ void c00000860(void)
  */
 void c00000865(void)
 {
+    int r, t;
+    scanf("%d", &r);
+    scanf("%d", &t);
+    int totalMin = 14400;
+    int result;
+    if (r == t) {
+        result = r + totalMin/150;
+    } else if (r < t) {
+        int diff = t-r;
+        int custoIgualar = diff*120;
+        if (custoIgualar <= totalMin) {
+            result = t + (totalMin-custoIgualar)/150;
+        } else {
+            result = r + totalMin/120;
+        }
+    } else {
+        int diff = r-t;
+        int custoIgualar = diff*30;
+        if (custoIgualar <= totalMin) {
+            result = r + (totalMin-custoIgualar)/150;
+        } else {
+            result = t + totalMin/30;
+        }
+    }
+    printf("%d", result);
 }
 
 /*
